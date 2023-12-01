1d17467a237ac09b1b2d9201ef2859b1220bb0d0
spikeoze <mukhtaaramiin@gmail.com>
2023-12-01 10:13:21 +0300

    docker initial app

---
+++ docker_app/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Hello Docker!'))
+app.listen(port, () => console.log(`listening on port ${port}!`))
