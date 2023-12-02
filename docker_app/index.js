03733dde059ebbdfa5d9f0e0ab189955484e6bb0
spikeoze <mukhtaaramiin@gmail.com>
2023-12-02 10:27:14 +0300

    docker basics

--- docker_app/index.js
+++ docker_app/index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello Docker!'))
+app.get('/', (req, res) => res.send('Hello Docker!!'))
 app.listen(port, () => console.log(`listening on port ${port}!`))
